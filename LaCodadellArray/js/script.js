3e74abbf2d081a56386a731db2de26ed4712f17e
afablelorenz <afablelorenz@gmail.com>
2024-05-15 19:31:38 +0200

    laCodaDellArray - updated script.js

--- LaCodadellArray/js/script.js
+++ LaCodadellArray/js/script.js
@@ -2,8 +2,13 @@ let arr = [];
 let numUtente;
 numUtente = Number.parseInt(prompt('Inserisci numero di elementi'),10)
 
-if(Number.isNan(numUtente) || numUtente < 5){
-    numUtente = 10;
+if(Number.isNaN(numUtente) || numUtente < 5){
+    numUtente = 5;
 }
 
-for
+for(let i=0; i < numUtente; i++){
+    const random = Math.floor(Math.random()*100)+1;
+    arr.push(random);
+}
+console.log(arr);
+console.log(arr[arr.length-1],arr[arr.length-2],arr[arr.length-3],arr[arr.length-4],arr[arr.length-5])
