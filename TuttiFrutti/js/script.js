2bc65fbf5960f73502b41cf594a23cf38c3a64fe
afablelorenz <afablelorenz@gmail.com>
2024-05-15 19:01:28 +0200

    TuttiFrutti - updated script

--- TuttiFrutti/js/script.js
+++ TuttiFrutti/js/script.js
@@ -1,3 +1,15 @@
 let arr = ['banana', 'mela', 'pera', 'ciliegia', 'arancia', 'mandarino', 'cocomero', 'limone', 'fragola'];
 arr.push('pesca');
+let c=false;
 
+for (let i=0; i<arr.length; i++){
+    if(arr[i]==='cocomero'){
+        c=true;
+    }
+}
+
+if(c){
+    console.log('Cocomero trovato');
+}else{
+    console.log('Cocomero non trovato');
+}
