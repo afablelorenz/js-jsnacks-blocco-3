9469e2c0f365ff9b9f3d1e9d3e79966e73d9d5bc
afablelorenz <afablelorenz@gmail.com>
2024-05-16 11:47:58 +0200

    updated script

--- snack-12/js/script.js
+++ snack-12/js/script.js
@@ -1,10 +1,13 @@
 let par1 = prompt('inserire una parola');
 let par2= prompt('inserire l\'altra parola');
 
+
 controlWords(par1,par2);
 
+
+
 function controlWords(word1,word2){
-    if (word1.length == word2.length){
+    if (word1.length === word2.length){
         console.log(word1,word2);
     }else if(word1.length > word2.length){
         console.log(word1);
