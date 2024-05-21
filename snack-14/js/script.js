7748890489951eef847401c84aedd6be19154de5
afablelorenz <afablelorenz@gmail.com>
2024-05-21 12:50:44 +0200

    update script (ho capito male la consegna)

--- snack-14/js/script.js
+++ snack-14/js/script.js
@@ -1,7 +1,7 @@
 // ! Chiedi all'utente un numero di secondi da attendere e 
 // ! fai partire un timer da quel momento a zero secondi, interrompendo il timer
 
-let  numSecUtente = Number.parseInt(prompt('Inserire i secondi'),10);
+/*let  numSecUtente = Number.parseInt(prompt('Inserire i secondi'),10);
 
 setTimeout(countSec,numSecUtente);
 
@@ -10,4 +10,21 @@ function countSec(){
     setInterval(function(){
         console.log(++i);
     },1000);
+}*/
+
+// Ho capito male la consegna
+
+let userSeconds = Number.parseInt(prompt('Scrivi i secondi che rimangono'),10);
+
+if(userSeconds > 10 || Number.isNaN(userSeconds)){
+    userSeconds = 5;
 }
+
+const timer= setInterval(function(){
+    userSeconds--;
+    console.log(userSeconds);
+    if(userSeconds <= 0){
+        clearInterval(timer);
+        alert('Buon anno');
+    }
+},1000);
