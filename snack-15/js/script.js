6589eee76519ba99bb3dfc3292d4bdfeb2ca2637
afablelorenz <afablelorenz@gmail.com>
2024-05-22 12:41:00 +0200

    snack-15

---
+++ snack-15/js/script.js
@@ -0,0 +1,9 @@
+let palla1 = {
+    nome: 'palla',
+    peso: '10'
+};
+
+let userPeso = Number.parseInt(prompt('Inserire il peso della pala'),10);
+console.log(palla1);
+palla1.peso = userPeso;
+console.log(palla1);
